ba48e440b249b6da6eb14f08ec2ddb651f5508f2
KevinLIOUST <kevin.lioust@outlook.fr>
2025-07-01 10:38:10 +0200

    Media Queries Terminées ! :)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 // Fonction pour générer des photos aléatoires à partir de lorem Picsum
 function genererPhotos(quantite) {
     for (let i = 0; i < quantite; i++) {
-        let lien = `https://picsum.photos/400?random=${i}`;
-        document.getElementById("gallerie-photos").innerHTML += `<img src=${lien} alt=\"https://picsum.photos/400?random=i\">`;
+        // let lien = `https://picsum.photos/${i}/1/200/300`;
+        // document.getElementById("gallerie-photos").innerHTML += `<a href="${lien}" data-lightbox="image-1" data-title="My caption" data-alt="Image non trouvée"><img class="m-3" src=${lien} alt=\"https://picsum.photos/${i}/1/200/300\"></a>`;
+        let lienRandom = `https://picsum.photos/400?random=${i}`;
+        document.getElementById("gallerie-photos").innerHTML += `<a href="${lienRandom}" data-lightbox="image-1" data-title="My caption" data-alt="Image non trouvée"><img class="m-3 imageGeneree" src=${lienRandom} alt=\"https://picsum.photos/400?random=${i}\"></a>`;
     }
 }
 
-genererPhotos(5);
+genererPhotos(30);
+
+// <img class="m-3" src=${lien} alt=\"https://picsum.photos/400?random=i\"> Random
+// <img class="m-3" src=${lien} alt=\"https://picsum.photos/id/400\"> id
